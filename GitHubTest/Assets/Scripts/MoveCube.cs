478d06828ae03b1ec56f4092013d0f80dd581307
dnjsrl33 <dnjsrl33@gmail.com>
2019-03-07 12:50:57 +0900

    CubeRotate
    Add CubeRotate Circle Movement

---
+++ GitHubTest/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+
+    public float speed;
+    public float maxZ = 1f;
+    public float moveZ = 0f;
+    public float moveY = 0f;
+    public float radius = 0f;
+    public float time;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime * speed;
+        moveZ = Mathf.Cos(time) * radius;
+        moveY = Mathf.Sin(time) * radius;
+        transform.position = new Vector3(moveZ, moveY);
+
+    }
+}
